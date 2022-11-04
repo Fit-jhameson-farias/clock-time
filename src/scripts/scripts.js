79af0c442e5ba2cbd7d64d1afa40cfcc25698677
fit-jhameson-farias <jhameson.farias@fitbank.com.br>
2022-11-04 11:09:50 -0300

    alter timeZone to America/Fortaleza

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -2,15 +2,13 @@ let hora = document.querySelector("#txtTime");
 
 function getHours() {
   const options = {
-    timeZone: "America/Vancouver",
+    timeZone: "America/Fortaleza",
     hour: "numeric",
     minute: "numeric",
     second: "numeric",
   };
   const date = new Intl.DateTimeFormat([], options);
-  // // console.log(date.format(new Date()));
   hora.innerText = date.format(new Date());
-  // alert();
 }
 
 setInterval(() => {
